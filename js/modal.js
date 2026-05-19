69871d71b1967fafd59f14dfde01213083f53a55
psyhoborsik-netizen <psyhoborsik@gmail.com>
2026-05-19 18:37:40 +0300

    added any modal window in project

--- js/modal.js
+++ js/modal.js
@@ -14,38 +14,12 @@
   }
 })();
 
-(() => {
-  const openModalBtns = document.querySelectorAll("[data-modal-open]");
-  const closeModalBtns = document.querySelectorAll("[data-modal-close]");
-
-  openModalBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-      const modalId = btn.getAttribute("data-modal-open");
-      const modal = document.getElementById(modalId);
-      if (modal) toggleModal(modal);
-    });
-  });
-
-  closeModalBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-      const modal = btn.closest("[data-modal]");
-      if (modal) toggleModal(modal);
-    });
-  });
-
-  function toggleModal(modal) {
-    modal.classList.toggle("is-hidden");
-    document.body.classList.toggle("no-scroll");
-  }
-})();
-
 document.addEventListener('DOMContentLoaded', () => {
   const openMenuBtn = document.querySelector('[data-menu-open]');
   const closeMenuBtn = document.querySelector('[data-menu-close]');
   const menu = document.querySelector('[data-menu]');
   const body = document.querySelector('body');
 
-  // ПРОВЕРКА: Нашел ли скрипт элементы на странице?
   console.log('Кнопка открытия:', openMenuBtn);
   console.log('Кнопка закрытия:', closeMenuBtn);
   console.log('Само меню:', menu);
@@ -64,3 +38,19 @@ document.addEventListener('DOMContentLoaded', () => {
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 });
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open-buyNow]"),
+    closeModalBtn: document.querySelector("[data-modal-close-buyNow]"),
+    modal: document.querySelector("[data-modal-buyNow]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
+  }
+})();
